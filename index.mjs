f1931f6125c0420ee1a6197319a2ba8e7388fb39
Donaldson10 <166642140+Donaldson10@users.noreply.github.com>
2024-12-08 22:03:29 -0800

    Squashed commit of the following:
    commit 60e71d3c617c5e357ceacc92e0ac7437ef832715
    Author: Donaldson10 <166642140+Donaldson10@users.noreply.github.com>
    Date:   Sun Dec 8 14:23:53 2024 -0800
    
        update
    
    commit b24eb29bc8634109d27ab77f076c96c769a6218b
    Author: Donaldson10 <166642140+Donaldson10@users.noreply.github.com>
    Date:   Sun Dec 8 10:10:58 2024 -0800
    
        update
    
    commit 43c199c6b05502f271949dd36657a66bdd7956ee
    Author: Donaldson10 <166642140+Donaldson10@users.noreply.github.com>
    Date:   Sat Dec 7 20:43:13 2024 -0800
    
        fetch

--- index.mjs
+++ index.mjs
@@ -1,5 +1,10 @@
-import express from 'express';
+import express, { json } from 'express';
 import mysql from 'mysql2/promise';
+import bcrypt from 'bcrypt';
+import session from 'express-session';
+import axios from 'axios';
+
+//const { NutritionAnalysisClient } = require('edamam-api');
 
 const app = express();
 
@@ -9,21 +14,25 @@ app.use(express.static('public'));
 //for Express to get values using POST method
 app.use(express.urlencoded({extended:true}));
 
-// setting up database connection pool
-// const pool = mysql.createPool({
-//     host: "your_hostname",
-//     user: "your_username",
-//     password: "your_password",
-//     database: "your_database",
-//     connectionLimit: 10,
-//     waitForConnections: true
-// });
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
 
+let authenticated = false;
+let loggedIn = false;
+let app_id = "b4286bf9";
+let app_key = "121bef8466d607b575f1c797f0a7905a";
+let searched = false;
+
+// setting up database connection pool
 const pool = mysql.createPool({
-    host: "aeckhaus.site",
-    user: "aeckhaus_webuser",
-    password: "Elkike#1",
-    database: "aeckhaus_quotes",
+    host: "yegord.tech",
+    user: "yegordte_webuser",
+    password: "cst-336",
+    database: "yegordte_foodLog",
     connectionLimit: 10,
     waitForConnections: true
 });
@@ -40,6 +49,92 @@ app.get('/mealplan',(req, res) => {
     res.render('mealplan')
  });
 
+ app.get('/nutrition', (req, res) => {
+    res.render('nutrition', {searched});
+ });
+
+ app.get('/meals', async (req, res) => {
+    let sql = `SELECT * FROM food`;
+    const [rows] = await conn.query(sql);
+
+    res.render('meals', {searched, rows});
+ });
+
+ app.get('/meals/edit', async (req, res) => {
+    let mealId = req.query.mealId;
+    let sql = `SELECT *
+                FROM food 
+                WHERE meal_Id = ?`;
+const [mealData] = await conn.query(sql, mealId);
+res.render('editMeals', {mealData});
+});
+
+app.post('/meals/edit', async (req, res) => {
+    let foodName = req.body.meal;
+    let url = await fetch(`https://api.edamam.com/api/nutrition-data?app_id=${app_id}&app_key=${app_key}&nutrition-type=logging&ingr=${foodName}`);
+    let nutrition = await url.json();
+
+    let calories = nutrition.calories;
+    let mealId = req.body.mealId;
+    let meal = req.body.meal;
+    let time = req.body.time;
+    let date = req.body.date;
+    let sql = `Update food
+                SET calories = ?, 
+                SET meal = ?,
+                SET time = ?,
+                SET date =?
+                WHERE meal_Id = ?`;
+    let params = [calories, meal, time, date, mealId];
+
+const [mealData] = await conn.query(sql, mealId);
+res.render('editMeals', {mealData});
+});
+
+
+app.post('/meals/delete', async (req, res) => {
+    let mealId = req.query.mealId;
+    let sql = `DELETE FROM food 
+                WHERE meal_Id = ?`;
+const [mealData] = await conn.query(sql, mealId);
+let sqlFood = `SELECT * FROM food`;
+    const [rows] = await conn.query(sqlFood);
+res.render('meals', {rows});
+});
+
+ app.post('/nutritionSearch', async (req, res) => {
+    let foodName = req.body.foodName;
+    console.log(foodName);
+    let url = await fetch(`https://api.edamam.com/api/nutrition-data?app_id=${app_id}&app_key=${app_key}&nutrition-type=logging&ingr=${foodName}`);
+    let nutrition = await url.json();
+
+    searched = true;
+
+    console.log(nutrition, nutrition.calories,nutrition.totalNutrients.FAT.quantity);
+    res.render('nutrition', {nutrition, searched});
+ });
+ app.post('/updateFoodLog', async (req, res) => {
+    let calories = req.body.calories;
+    let meal = req.body.meal;
+    let time = req.body.time;
+    let date = req.body.date;
+    console.log(calories);
+    console.log(meal);
+    console.log(time);
+    console.log(date);
+
+
+
+    let sql = `INSERT INTO food
+                (calories, meal, time, date)
+                VALUES
+                (?,?,?,?)`;
+    let params = [calories, meal, time, date];
+    const [rows] = await conn.query(sql, params);
+    searched = false;
+    res.render('nutrition',{searched})
+ });
+
 app.get("/dbTest", async(req, res) => {
     let sql = "SELECT CURDATE()";
     const [rows] = await conn.query(sql);
@@ -49,6 +144,5 @@ app.get("/dbTest", async(req, res) => {
 app.listen(3000, ()=>{
     console.log("Express server running")
 })
-
 
 
