d5dbf4f0faa96d603a28a30f3045ea108eb12f38
Alan23111 <119461603+Alan23111@users.noreply.github.com>
2024-12-08 22:12:50 -0800

    styles and authentication

--- index.mjs
+++ index.mjs
@@ -28,18 +28,141 @@ const pool = mysql.createPool({
     waitForConnections: true
 });
 
-
 const conn = await pool.getConnection();
 
-//routes
 app.get('/', (req, res) => {
-   res.render('home')
+    res.render('login'); 
 });
 
-app.get('/mealplan',(req, res) => {
+app.post('/login', async (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    let passwordHash = "";
+    let sql = `SELECT *
+               FROM admin
+               WHERE username = ?`;
+    const [rows] = await conn.query(sql, [username]);
+    if (rows.length > 0) {
+        passwordHash = rows[0].password;
+    }
+    let match = await bcrypt.compare(password, passwordHash);
+    if (match) {
+        req.session.fullName = rows[0].firstName + " " + rows[0].lastName;
+        req.session.authenticated = true;
+        return res.render('home');
+    } else {
+        return res.redirect("/");
+    }
+});
+
+function isAuthenticated(req, res,next){
+    if (req.session.authenticated){
+        next();
+    }else{
+        res.redirect("/");
+    }}
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/'); 
+    });
+});
+
+app.get('/home', isAuthenticated, (req, res) => {
+    res.render('home'); 
+});
+
+app.get('/mealplan', isAuthenticated, (req, res) => {
     res.render('mealplan')
  });
 
+<<<<<<< Updated upstream
+=======
+ app.get('/nutrition', isAuthenticated, (req, res) => {
+    res.render('nutrition', {searched});
+ });
+
+ app.get('/meals', isAuthenticated,  async (req, res) => {
+    let sql = `SELECT * FROM food`;
+    const [rows] = await conn.query(sql);
+
+    res.render('meals', {searched, rows});
+ });
+
+ app.get('/meals/edit', isAuthenticated, async (req, res) => {
+    let mealId = req.query.mealId;
+    let sql = `SELECT *
+                FROM food 
+                WHERE meal_Id = ?`;
+const [mealData] = await conn.query(sql, mealId);
+res.render('editMeals', {mealData});
+});
+
+app.post('/meals/edit', isAuthenticated, async (req, res) => {
+    let foodName = req.body.meal;
+    let url = await fetch(`https://api.edamam.com/api/nutrition-data?app_id=${app_id}&app_key=${app_key}&nutrition-type=logging&ingr=${foodName}`);
+    let nutrition = await url.json();
+
+    let calories = nutrition.calories;
+    let mealId = req.body.mealId;
+    let meal = req.body.meal;
+    let time = req.body.time;
+    let date = req.body.date;
+    let sql = `Update food
+                SET calories = ?, 
+                SET meal = ?,
+                SET time = ?,
+                SET date =?
+                WHERE meal_Id = ?`;
+    let params = [calories, meal, time, date, mealId];
+
+const [mealData] = await conn.query(sql, mealId);
+res.render('editMeals', {mealData});
+});
+
+
+app.post('/meals/delete', isAuthenticated, async (req, res) => {
+    let mealId = req.query.mealId;
+    let sql = `DELETE FROM food 
+                WHERE meal_Id = ?`;
+const [mealData] = await conn.query(sql, mealId);
+let sqlFood = `SELECT * FROM food`;
+    const [rows] = await conn.query(sqlFood);
+res.render('meals', {rows});
+});
+
+ app.post('/nutritionSearch', isAuthenticated, async (req, res) => {
+    let foodName = req.body.foodName;
+    console.log(foodName);
+    let url = await fetch(`https://api.edamam.com/api/nutrition-data?app_id=${app_id}&app_key=${app_key}&nutrition-type=logging&ingr=${foodName}`);
+    let nutrition = await url.json();
+
+    searched = true;
+
+    console.log(nutrition, nutrition.calories,nutrition.totalNutrients.FAT.quantity);
+    res.render('nutrition', {nutrition, searched});
+ });
+ app.post('/updateFoodLog', isAuthenticated, async (req, res) => {
+    let calories = req.body.calories;
+    let meal = req.body.meal;
+    let time = req.body.time;
+    let date = req.body.date;
+    console.log(calories);
+    console.log(meal);
+    console.log(time);
+    console.log(date);
+
+    let sql = `INSERT INTO food
+                (calories, meal, time, date)
+                VALUES
+                (?,?,?,?)`;
+    let params = [calories, meal, time, date];
+    const [rows] = await conn.query(sql, params);
+    searched = false;
+    res.render('nutrition',{searched})
+ });
+
+>>>>>>> Stashed changes
 app.get("/dbTest", async(req, res) => {
     let sql = "SELECT CURDATE()";
     const [rows] = await conn.query(sql);
@@ -49,6 +172,9 @@ app.get("/dbTest", async(req, res) => {
 app.listen(3000, ()=>{
     console.log("Express server running")
 })
+<<<<<<< Updated upstream
 
 
 
+=======
+>>>>>>> Stashed changes
